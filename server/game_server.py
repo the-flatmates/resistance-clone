4ea09de438bda6ae8db2ba752fdf05adc4bc4cca
tlazor <tjlazor@vt.edu>
2018-05-12 01:55:19 -0500

    added client/host initial info retrieval

--- server/game_server.py
+++ server/game_server.py
@@ -1,33 +1,67 @@
 #!/usr/bin/python3
 
-import socket, socketserver
+import socket
+import socketserver
+import threading
+
 
 class ResistanceCloneServer(socketserver.BaseRequestHandler):
-	def handle(self):
-		self.data = self.request.recv(1024).strip()
-		print("{0} wrote: {1}".format(self.client_address[0], self.data))
-		self.request.sendall(self.data.upper())
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
 
-	def CreateGame():
-		AcceptHost()
-		AcceptClients()
+        self.clients = []
 
-	def AcceptClients():
-		pass
+    def getClientData(self, client, address):
+            # todo add a client class
+        clientName = client.recv(32).strip()
+        roomCode = client.recv(6).strip()
+        print("{0}'s name is {1} and their code is {2}".format(
+            client, clientName, roomCode))
 
-	def AcceptHost():
-		pass
-		self.settings = dict{targeting=targeting, 
-							idiotProof=idiotProof,
-							blindSpies=blindSpies,
-							colorBlind=colorBlind,
-							spyRevealPrompt=spyRevealPrompt,
-							numPlayers=numPlayers}
+        self.clients.append("add client class")
+
+    def acceptClients(self):
+        self.sock.listen(10)
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(60)
+            threading.Thread(target=self.getClientData,
+                             args=(client, address)).start()
+
+    def acceptHost(self):
+        self.sock.listen(1)
+        client, address = self.sock.accept()
+
+        # todo add host class? maybe just a normal client
+        targeting = client.recv(1)
+        idiotProof = client.recv(1)
+        blindSpies = client.recv(1)
+        colorBlind = client.recv(1)
+        spyRevealPrompt = client.recv(1)
+        numPlayers = client.recv(1)
+
+        self.getClientData(client, address)
+
+        self.settings = {"targeting": targeting,
+                         "idiotProof": idiotProof,
+                         "blindSpies": blindSpies,
+                         "colorBlind": colorBlind,
+                         "spyRevealPrompt": spyRevealPrompt,
+                         "numPlayers": numPlayers}
+
+    def createGame(self):
+        self.acceptHost()
+        self.acceptClients()
+
 
 if __name__ == "__main__":
-	HOST, PORT = "10.142.0.2", 9998
+    HOST, PORT = "10.142.0.2", 9998
 
-	print("Listening on {0}:{1}".format(HOST, PORT))
+    print("Listening on {0}:{1}".format(HOST, PORT))
 
-	server = socketserver.TCPServer((HOST, PORT), TcpHandler)
-	server.serve_forever()
+    server = socketserver.TCPServer((HOST, PORT), TcpHandler)
+    server.serve_forever()
