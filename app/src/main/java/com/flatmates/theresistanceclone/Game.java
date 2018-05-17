998be0cce7c47af5fa604f0390081148bd8b0c2a
eqnguyen <nguyen.eric@hotmail.com>
2018-05-16 23:57:29 -0400

    Add class for game variables

---
+++ app/src/main/java/com/flatmates/theresistanceclone/Game.java
@@ -0,0 +1,13 @@
+package com.flatmates.theresistanceclone;
+
+public class Game {
+    private static String room_code;
+    private static String player_name;
+    private static int num_players;
+    private static String [] player_names;
+    private static boolean targeting;
+    private static boolean idiot_proof;
+    private static boolean blind_spies;
+    private static boolean spy_reveal;
+    private static boolean color_blind;
+}
