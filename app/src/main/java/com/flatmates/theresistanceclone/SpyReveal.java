3a745140cf66cbb19da7114e570939d01e28a98c
eqnguyen <nguyen.eric@hotmail.com>
2018-05-17 02:09:04 -0400

    Add text to speech capability for spy reveal prompt

--- app/src/main/java/com/flatmates/theresistanceclone/SpyReveal.java
+++ app/src/main/java/com/flatmates/theresistanceclone/SpyReveal.java
@@ -1,13 +1,36 @@
 package com.flatmates.theresistanceclone;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class SpyReveal extends AppCompatActivity {
+    TextToSpeech tts;
+    TextView prompt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spy_reveal);
+
+        prompt = findViewById(R.id.tv_reveal_prompt);
+
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    tts.setLanguage(Locale.US);
+                }
+            }
+        });
+    }
+
+    public void readPrompt(View view) {
+        String toSpeak = prompt.getText().toString();
+        tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
     }
 }
