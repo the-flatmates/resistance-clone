ccfd37eaba2a099f4dc04dfb9494b648f4319d26
eqnguyen <nguyen.eric@hotmail.com>
2018-05-18 23:06:43 -0400

    Send start signal to server on button click

--- app/src/main/java/com/flatmates/theresistanceclone/SpyReveal.java
+++ app/src/main/java/com/flatmates/theresistanceclone/SpyReveal.java
@@ -1,5 +1,6 @@
 package com.flatmates.theresistanceclone;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
@@ -32,5 +33,18 @@ public class SpyReveal extends AppCompatActivity {
     public void readPrompt(View view) {
         String toSpeak = prompt.getText().toString();
         tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    public void sendStart(View view) {
+        ClientSend c = new ClientSend();
+        String[] params = {"s000"};
+        try {
+            c.execute(params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // TODO: Determine if leader or not, then start mission screen
+//        Intent intent = new Intent(Wait.this, Mission.class);
+//        startActivity(intent);
     }
 }
