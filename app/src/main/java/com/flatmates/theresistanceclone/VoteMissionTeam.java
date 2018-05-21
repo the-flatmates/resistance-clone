be1065cd54af33961505b39d21fd247116a9d275
eqnguyen <nguyen.eric@hotmail.com>
2018-05-21 01:09:36 -0400

    Add activities for selecting team, voting on team, and going on mission

---
+++ app/src/main/java/com/flatmates/theresistanceclone/VoteMissionTeam.java
@@ -0,0 +1,13 @@
+package com.flatmates.theresistanceclone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VoteMissionTeam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vote_mission_team);
+    }
+}
