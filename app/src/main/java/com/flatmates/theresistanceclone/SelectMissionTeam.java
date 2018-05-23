3ee4ead72d1e67bd6ca7e892f6f69d968fce94ed
Eric Nguyen <eric.nguyen@jhuapl.edu>
2018-05-22 21:30:19 -0400

    Initialize vote tracker, mission info, and toggle buttons for mission select

--- app/src/main/java/com/flatmates/theresistanceclone/SelectMissionTeam.java
+++ app/src/main/java/com/flatmates/theresistanceclone/SelectMissionTeam.java
@@ -1,7 +1,11 @@
 package com.flatmates.theresistanceclone;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class SelectMissionTeam extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class SelectMissionTeam extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_mission_team);
+
+        setVoteTrack();
+        setMissionInfo();
+        listPlayerNames();
+    }
+
+    private void setVoteTrack() {
+        LinearLayout ll_vote_tracker = findViewById(R.id.ll_vote_tracker);
+        TextView tv_vote_track = (TextView) ll_vote_tracker.getChildAt(Game.getVoteTrack() - 1);
+        tv_vote_track.setTextColor(Color.RED);
+    }
+
+    private void setMissionInfo() {
+        TextView tv_mission_info = findViewById(R.id.tv_mission_info);
+        String missionSize = Integer.toString(Game.getMissionInfo().get(Game.getMission())[0]);
+        String numFails = Integer.toString(Game.getMissionInfo().get(Game.getMission())[1]);
+        String missionInfo = missionSize + " (" + numFails + " Fail)";
+        tv_mission_info.setText(missionInfo);
+    }
+
+    private void listPlayerNames() {
+        LinearLayout ll_team_selection = findViewById(R.id.ll_team_selection);
+        for (String playerName : Game.getLeaderOrder()) {
+            ToggleButton toggleButton = new ToggleButton(this);
+            toggleButton.setText(playerName);
+            toggleButton.setTextOff(playerName);
+            toggleButton.setTextOn(playerName);
+            ll_team_selection.addView(toggleButton);
+        }
     }
 }
